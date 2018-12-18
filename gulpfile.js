f2aa28ca31ca7593a73de9000b6f0bda4ff22345
mkasumi <morita@appleple.com>
2018-12-19 08:21:14 +0900

    Update: build コマンドを追加

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,6 @@ var browserSync = require('browser-sync').create(),
 	gzip = require('gulp-gzip'),
     changedInPlace = require('gulp-changed-in-place');
 
-// プロジェクト設定
-// var rootTheme = 'takisada';
-
 //テーマ
 //SCSSファイルをCSSにコンパイルする
 gulp.task('sass', function () {
@@ -37,27 +34,7 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('css'))
 		.pipe(browserSync.stream());
 });
-// // SCSSファイルを圧縮する
-// SCSSファイルを圧縮するgulp.task('mincss', function () {
-// 	gulp.src(['themes/' + rootTheme + '/scss/site.scss'])
-// 		.pipe(rename({
-// 			suffix: '.min'
-// 		}))
-// 		.pipe(sass())
-// 		.pipe(autoprefixer({
-// 			browsers: ['last 2 versions', 'ie 9'],
-// 			cascade: false
-// 		}))
-// 		.pipe(csscomb())// *1
-// 		.pipe(gzip())
-// 		.pipe(gulp.dest('themes/' + rootTheme + '/css'));
-// });
 
-// gulp.task('minjs', function () {
-//	gulp.src(['themes/' + rootTheme + '/js/**.js'])
-//		.pipe(gzip())
-//		.pipe(gulp.dest('themes/' + rootTheme + '/js'));
-// });
 
 gulp.task('html', function () {
 	gulp.src(['**.html'])
@@ -65,21 +42,14 @@ gulp.task('html', function () {
 });
 
 
-
 // プロジェクトのSCSSとCSSファイルを監視する
 gulp.task('project', function() {
 	gulp.watch('scss/**',['sass']);
-	// gulp.watch('themes/' + rootTheme + '/css/site.css',['mincss']);
-  	// gulp.watch('themes/' + rootTheme + '/js/**.js',['minjs']);
 });
 
-// gulp.task('acms', function() {
-// 	gulp.watch('themes/' + rootTheme + '/scss/foundation/acms/**',['acmssass']);
-// 	gulp.watch('themes/' + rootTheme + '/css/acms.css',['acmsmin']);
-// });
 
 // デフォルトのタスク
 gulp.task('default', ['project']);
 
-
-// *1・*2 gulp-csscombとgulp-sourcemaps は、gulp-csscombがgulp-sourcemapsに対応していないので、同時に動作することができません。gulp-csscombを動作させたい場合は、「*2」の記述をコメントアウトしてください。
+// buildのタスク
+gulp.task('compile', ['sass']);
